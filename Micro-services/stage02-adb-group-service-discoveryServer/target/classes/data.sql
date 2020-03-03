2e1b709307feb0429df3c62bd62809343f01fe51
Soumya-gadashetti <60088012+Soumya-gadashetti@users.noreply.github.com>
2020-03-02 20:21:15 -0800

    Add files via upload

---
+++ Micro-services/stage02-adb-group-service-discoveryServer/target/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO groups
+VALUES('FAMILY','FAMILY Members','All my Family members');
+INSERT INTO groups
+VALUES('WORK','My WORK Mates','ALL My Work MAtes');
+INSERT INTO groups
+VALUES('Gym','Gym Friends','All My Gym Friends');
