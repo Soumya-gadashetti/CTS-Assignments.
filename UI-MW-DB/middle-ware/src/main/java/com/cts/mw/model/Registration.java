bf2d2a8e8ad9ca738214a54e692d68c16dffd877
Soumya-gadashetti <60088012+Soumya-gadashetti@users.noreply.github.com>
2020-03-05 03:48:19 -0800

    Middle-Ware to Database

---
+++ UI-MW-DB/middle-ware/src/main/java/com/cts/mw/model/Registration.java
@@ -0,0 +1,59 @@
+package com.cts.mw.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class Registration {
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String  username;
+	private String mobileNumber;
+	private String eMail;
+	private String password;
+	private String confirmPassword;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String geteMail() {
+		return eMail;
+	}
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+	
+	
+
+}
